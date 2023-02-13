441390eb9376a3ba4bc83efcc63afa210290c6a7
itmagnat-ru <117728092+itmagnat-ru@users.noreply.github.com>
2023-02-13 16:46:27 +0400

    tst

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -1,18 +1,25 @@
 package hiber.dao;
 
 import hiber.model.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
 public class UserDaoImp implements UserDao {
 
-   @Autowired
-   private SessionFactory sessionFactory;
+   private final SessionFactory sessionFactory;
+
+   public UserDaoImp(SessionFactory sessionFactory) {
+      this.sessionFactory = sessionFactory;
+   }
 
    @Override
    public void add(User user) {
@@ -25,5 +32,16 @@ public class UserDaoImp implements UserDao {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
-
+   @Override
+   @SuppressWarnings("unchecked")
+   public User getUserByCar(String model, int series) {
+      String hql = "from User user where user.car.model = :model and user.car.series = :series";
+      try {
+         TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
+         query.setParameter("model", model).setParameter("series", series);
+         return query.setMaxResults(1).getSingleResult();
+      } catch (NoResultException ex) {
+         return null;
+      }
+   }
 }
--- 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/service/UserService.java
@@ -1,10 +1,12 @@
 package hiber.service;
 
 import hiber.model.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface UserService {
     void add(User user);
     List<User> listUsers();
+    User getUserByCar(String model, int series);
 }
